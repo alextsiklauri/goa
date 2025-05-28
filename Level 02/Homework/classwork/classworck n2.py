fb143c416e9c5195ed35d14f18853ce2475f84ed
alextsiklauri <Tsiklaurialex1@gmail.com>
2025-05-29 01:02:53 +0400

    first commit

---
+++ Level 02/Homework/classwork/classworck n2.py
@@ -0,0 +1,16 @@
+# ცვლადი გვჭირდება იმაში რომ რაღცა რომ შევინახოთ და მერე დიდი დრო რომ გავა დავწერტ ცვლადის სახელს და ამოგვიგდებს თუ რა შევინახეთ ცვლადში
+
+number1 = 5
+
+number2 = 10
+print (number1 + number2)
+print (number1 - number2)
+print (number1 * number2)
+print (number1 / number2)
+surname = "wiklauri"
+age  = 11
+print (surname * age)
+age2  = "11"
+name = "alex"
+hight = "160cm"
+print( "hello my name is"+name+" i am "+hight+" my surnameis "+surname+" im "+age2)
