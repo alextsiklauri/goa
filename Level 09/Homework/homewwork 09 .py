a13e721232fd6dc104eac181e5991775fd47ecd0
alextsiklauri <Tsiklaurialex1@gmail.com>
2025-06-26 18:07:31 +0400

    commit

---
+++ Level 09/Homework/homewwork 09 .py
@@ -0,0 +1,10 @@
+number = input("enter a number")
+number = input("enter a number")
+print(number)
+name = "alex"
+user_name = input("enter your name")
+print(name + user_name)
+name = "alex"
+num1 = 24234
+print(name * num1)
+print(number)
---
+++ Level 09/classwork/classwork 09 .py
@@ -0,0 +1,23 @@
+#კონკატინაცია არის ორი სიტყვის ან ორი წინადადების ანუ სტრინგის მიმატება
+#name = "giorgi"
+#age = 15 
+
+#print(name + age)
+#ეს კოდი რაც მაღ₾ა წერია გამოიტანს ერორს რადგან სტრინგს ინტენგერს ვერ მივუმატებთ
+float = 2.0
+interger = 2
+string = "2"
+list = "red"
+print(float)
+print(interger)
+print(string)
+print(list)
+
+num1 = 424
+num2 = 234
+print(num1 / num2)
+#num1 num2ზე რომ გავყე მივიღე float მონაცემთა ტიპი
+
+word = "projector"
+num = 32
+print(word * num)
