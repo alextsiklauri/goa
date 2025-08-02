13bd8e1dd792fa6f196c790fec1bdab3b515df57
alextsiklauri <Tsiklaurialex1@gmail.com>
2025-08-02 15:17:23 +0400

    commit

---
+++ Level 11/Classwork/level 11 .py
@@ -0,0 +1,56 @@
+print(11>12)
+#true
+print(11>10)
+#true
+print(11>9)
+#true
+print(11>8)
+#true
+print(11>7)
+#true
+print(11>6)
+#true
+print(11>5)
+#true
+print(11>4)
+#true
+print(11>3)
+#true
+print(11>2)
+#true
+print(11>12)
+#flase
+print(11>13)
+#flase
+print(11>14)
+#flase
+print(11>15)
+#flase
+print(11>16)
+#flase
+print(11>17)
+#flase
+print(11>18)
+#flase
+print(11>19)
+#flase
+print(11>20)
+#flase
+print(11>21)
+#flase
+#boolean არის ture flase 
+#კომპი False and true) #false
+print(Ture and False) #flase
+print(truen and True) #ture
+print(false and false) #false
+print(false or true) #true
+print(true or false) #true
+print(true or true) #true
+print(false or false) #flase
+print(flase and 30>20 )
+print(true and 40>2 )
+num1 = 23+10
+print(num1 )
+num1>11 
+tempeture = 30
+31 > tempeture
